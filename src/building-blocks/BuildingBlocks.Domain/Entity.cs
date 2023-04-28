511f6f97e6060dff434d0aa6c6b9bc32e8030af9
wiaoj <bertandeniz7@gmail.com>
2023-04-28 14:38:16 +0300

    added base entity

---
+++ src/building-blocks/BuildingBlocks.Domain/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildingBlocks.Domain;
+public abstract class Entity {
+	public Guid Id { get; private set; }
+	public DateTime CreatedAt { get; private set; }
+
+	public Entity() {
+		this.Id = Guid.NewGuid();
+		this.CreatedAt = DateTime.UtcNow;
+	}
+}
