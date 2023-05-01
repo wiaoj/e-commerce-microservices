6e1e67692dbc8cf2d23da48a1946c37282ec316c
wiaoj <-h>
2023-05-01 19:25:47 +0300

    added context

---
+++ src/services/catalog-service/CatalogService.Persistence/Contexts/CategoryServiceDbContext.cs
@@ -0,0 +1,7 @@
+﻿using BuildingBlocks.Persistence.EFCore.MSSQL.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogService.Persistence.Contexts;
+public sealed class CategoryServiceDbContext : EFCoreMSSQLDbContext {
+	public CategoryServiceDbContext(DbContextOptions options) : base(options) {	}
+}
