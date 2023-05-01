433580bc9c3f6a6730a66bce82ae6adbb6d13e57
wiaoj <-h>
2023-05-01 19:27:46 +0300

    added category repositories

---
+++ src/services/catalog-service/CatalogService.Persistence/Repositories/CategoryReadRepository.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.Persistence.EFCore.Repositories;
+using CatalogService.Domain.Entities;
+using CatalogService.Persistence.Contexts;
+using CatalogService.Persistence.Repositories.Interfaces;
+
+namespace CatalogService.Persistence.Repositories;
+public sealed class CategoryReadRepository : EFAsyncReadRepository<CategoryEntity, CategoryServiceDbContext>, ICategoryReadRepository {
+	public CategoryReadRepository(CategoryServiceDbContext context) : base(context) { }
+}
---
+++ src/services/catalog-service/CatalogService.Persistence/Repositories/CategoryWriteRepository.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.Persistence.EFCore.Repositories;
+using CatalogService.Domain.Entities;
+using CatalogService.Persistence.Contexts;
+using CatalogService.Persistence.Repositories.Interfaces;
+
+namespace CatalogService.Persistence.Repositories;
+public sealed class CategoryWriteRepository : EFAsyncWriteRepository<CategoryEntity, CategoryServiceDbContext>, ICategoryWriteRepository {
+	public CategoryWriteRepository(CategoryServiceDbContext context) : base(context) { }
+}
