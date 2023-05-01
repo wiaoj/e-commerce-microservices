68204fdbde973191dc963eef0385ff3fad773488
wiaoj <-h>
2023-05-01 18:11:52 +0300

    category entity added

---
+++ src/services/catalog-service/CatalogService.Domain/Entities/CategoryEntity.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.Domain;
+
+namespace CatalogService.Domain.Entities;
+public sealed class CategoryEntity : Entity {
+	public required String Name { get; set; }
+	public Guid? ParentCategoryId { get; set; }
+	public CategoryEntity? ParentCategory { get; set; }
+
+	public CategoryEntity(String name) {
+		ArgumentException.ThrowIfNullOrEmpty(name, "Kategori ismi boş olamaz!");
+		this.Name = name;
+	}
+
+	public CategoryEntity(String name, CategoryEntity parentCategory) : this(name) {
+		ArgumentNullException.ThrowIfNull(parentCategory, "Üst kategori bulunamadı!");
+		this.ParentCategory = parentCategory;
+		this.ParentCategoryId = parentCategory.Id;
+	}
+}
