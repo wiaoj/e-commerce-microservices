d2585fcb9e13fc1e70dd14b2042632414a92fae4
wiaoj <-h>
2023-05-07 15:06:32 +0300

    added default value at pagination

--- src/building-blocks/BuildingBlocks.Application/Pagination/PaginationRequest.cs
+++ src/building-blocks/BuildingBlocks.Application/Pagination/PaginationRequest.cs
@@ -1,3 +1,11 @@
 ﻿namespace BuildingBlocks.Application.Pagination;
-public sealed record PaginationRequest(Int32 Page, Int32 Size);
+public sealed record PaginationRequest {
+	public UInt32 Page { get; set; }
+	public UInt64 Size { get; set; }
+
+	public PaginationRequest() {
+		this.Page = 1;
+		this.Size = 10;
+	}
+};
 public sealed record PaginationResponse<T>() : Paginate<T>;
