cc801ae8f12a2befdc47ed1b6822c0ee237ac0dc
wiaoj <-h>
2023-05-07 19:28:25 +0300

    added pagination info

--- src/services/catalog-service/CatalogService.Application/Features/Categories/Dtos/GetCategoryDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CatalogService.Application.Features.Categories.Dtos;
-public sealed record GetCategoryDto {
-	public Guid Id { get; set; }
-	public String Name { get; set; } = null!;
-	public Guid ParentCategoryId { get; set; } = Guid.Empty;
-	public List<GetCategoryDto> ChildCategories { get; set; } = new();
-}
--- src/services/catalog-service/CatalogService.Persistence/Repositories/CategoryWriteRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BuildingBlocks.Persistence.EFCore.Repositories;
-using CatalogService.Domain.Entities;
-using CatalogService.Persistence.Contexts;
-using CatalogService.Persistence.Repositories.Interfaces;
-
-namespace CatalogService.Persistence.Repositories;
-public sealed class CategoryWriteRepository : EFAsyncWriteRepository<CategoryEntity, CategoryServiceDbContext>, ICategoryWriteRepository {
-	public CategoryWriteRepository(CategoryServiceDbContext context) : base(context) { }
-}
